8a31750fd4fc0f3e2381740f779f78d7d803c522
CutyApple <dbtldhs0915@naver.com>
2021-03-14 22:41:42 +0900

    [FEAT] <features/Post/PostListItem> Apply Props

---
+++ src/features/Post/PostListItem/PostListItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./style";
+
+const PostListItem = ({ userId, id, title }) => {
+  return <S.Container>{title}</S.Container>;
+};
+
+export default PostListItem;
