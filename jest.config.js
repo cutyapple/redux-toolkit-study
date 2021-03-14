b0b5f2cf5cd03b0525088f31af666cb65a6ee812
CutyApple <dbtldhs0915@naver.com>
2021-03-15 08:54:50 +0900

    [CHORE] <jest.config.js>

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/__test__/config/importJestDOM.js"],
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "json"],
+  transform: {
+    "^.+\\.(js|jsx)?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  testMatch: [
+    "<rootDir>/**/*.test.(js|jsx|ts|tsx)",
+    "<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))",
+  ],
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
 };
