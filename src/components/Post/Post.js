911a0cacd2ba2a61839f2f61d531b5555ce502d1
CutyApple <dbtldhs0915@naver.com>
2021-03-14 22:37:57 +0900

    [FEAT] <components/Post> Apply Container

---
+++ src/components/Post/Post.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as S from "./style";
+
+import PostListContainer from "../../features/Post/PostList/PostListContainer";
+
+const Post = () => {
+  return (
+    <S.Container>
+      <PostListContainer />
+    </S.Container>
+  );
+};
+
+export default Post;
