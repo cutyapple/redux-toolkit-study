3208b50b3e57c5f8b6ac7b76938344b08f56b5c0
CutyApple <dbtldhs0915@naver.com>
2021-03-14 22:42:43 +0900

    [FEAT] <src/App.js> Add "Post" Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Post from "./components/Post/Post";
 import Todo from "./components/Todo/Todo";
 
 const App = () => {
-  return <Todo />;
+  return (
+    <>
+      <Todo />
+      <Post />
+    </>
+  );
 };
 
 export default App;
