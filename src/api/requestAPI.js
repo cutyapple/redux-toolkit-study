caab2b040b462ac582935ba0b2350c966991b7e2
CutyApple <dbtldhs0915@naver.com>
2021-03-14 22:37:12 +0900

    [FEAT] <src/api/requestAPI.js> Add "requestApi"

---
+++ src/api/requestAPI.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+export const methodType = {
+  GET: "get",
+  POST: "post",
+  PUT: "put",
+  PATCH: "patch",
+  DELETE: "delete",
+};
+
+export const requestApi = async (method, url, body, headers) => {
+  try {
+    const res = await axios[method](BASE_URL + url, body, {
+      headers,
+    });
+
+    return res;
+  } catch (error) {
+    console.log(`requestApi/requestApi`);
+    console.log(error);
+  }
+};
