8093faef09da4ca56544ab21a8752db4e81ef82e
유시온 <dbtldhs0915@naver.com>
2021-03-16 07:55:43 +0900

    [FEAT] <src/components/Todo/Todo.js> Apply TodoListContainer

---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoListContainer from "../../features/Todo/TodoList/Container";
+
+const Todo = () => {
+  return (
+    <div>
+      <TodoListContainer />
+    </div>
+  );
+};
+
+export default Todo;
