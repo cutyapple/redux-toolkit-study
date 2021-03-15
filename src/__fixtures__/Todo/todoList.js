b4f31e056ff3fa9fe6d6781a8bb45d360851eccf
유시온 <dbtldhs0915@naver.com>
2021-03-16 07:57:38 +0900

    [TEST] <src/__fixtures__/Todo/todoList.js>

---
+++ src/__fixtures__/Todo/todoList.js
@@ -0,0 +1,14 @@
+const todoListFixtures = [
+  {
+    id: 1,
+    title: "오감자 먹기",
+    done: false,
+  },
+  {
+    id: 2,
+    title: "오감자 팔기",
+    done: false,
+  },
+];
+
+export default todoListFixtures;
